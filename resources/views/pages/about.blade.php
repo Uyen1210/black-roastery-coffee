a9c3f40d056fb3519e2133b269486297c137bace
Ly2303-hub <nguyentrancamly230305@gmail.com>
2025-10-18 02:15:09 +0700

    add about file

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Giới Thiệu')
+
+@section('content')
+    <h2>The Gemini Roastery</h2>
+    <p>
+        Neyu Roastery Coffee ra đời với sứ mệnh...
+    </p>
+@endsection
