4f5a3b9f3c3faf545786179ded05902f334ec552
Ly2303-hub <nguyentrancamly230305@gmail.com>
2025-10-18 14:04:20 +0700

    update about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,8 +3,20 @@
 @section('title', 'Giới Thiệu')
 
 @section('content')
-    <h2>The Gemini Roastery</h2>
+<h2>The Universe Roastery</h2>
     <p>
-        Neyu Roastery Coffee ra đời với sứ mệnh...
+        <b>Black Roastery Coffee </b> ra đời với sứ mệnh kiến tạo một không gian đậm chất riêng – nơi chất cà phê nguyên bản
+        được
+        tôn vinh qua phong cách tối giản và tinh tế. Lấy cảm hứng từ gam màu đen – biểu tượng của sự mạnh mẽ, bí ẩn và khẳng
+        định cá tính – Black Roastery Coffee không chỉ phục vụ thức uống, mà còn truyền tải một lối sống đề cao chất lượng
+        và trải nghiệm.
+    </p>
+    </br>
+    <p>
+        Thực đơn của quán được chọn lọc với 6 món chủ đạo, thể hiện sự kết hợp giữa cổ điển và sáng tạo:<b> Latte,
+            Macchiato,
+            Americano, Matcha xoài, Trà sữa cốm </b> cùng các biến tấu đặc trưng khác. Mỗi ly nước là sự hòa quyện giữa
+        hương vị
+        chuẩn mực và dấu ấn riêng, được tạo nên từ tinh thần của Black: tối giản nhưng khác biệt, đơn sắc nhưng chiều sâu.
     </p>
 @endsection
